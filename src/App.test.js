cac5977b5c1eb7d20709ba974917f6682c0d332e
Zahedh Hossain <zhossain@paloaltonetworks.com>
2018-11-16 16:19:49 -0800

    Test CI

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
+const app = shallow(<App />);
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Render Properly via enzyme', () => {
+  expect(app).toMatchSnapshot();
 });
